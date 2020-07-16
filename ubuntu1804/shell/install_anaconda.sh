2ad22957677243e7f0072946d7940c1666a94331
okojomoeko <chikoro.size@gmail.com>
2020-07-16 14:18:57 +0000

    add auto install anaconda

--- ubuntu1804/shell/install_anaconda.sh
+++ ubuntu1804/shell/install_anaconda.sh
@@ -1,10 +1,7 @@
-#!/bin/sh
-# Install Anaconda for yourself
-wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh -P $1
 
 expect -c "
 set timeout 360
-spawn /bin/sh $1/Anaconda3-2020.02-Linux-x86_64.sh
+spawn /bin/sh Anaconda3-2020.02-Linux-x86_64.sh
 expect \">>>\"
 send \"\n\"
 expect \":\"
