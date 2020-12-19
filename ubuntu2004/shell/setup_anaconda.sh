645809d186d4dbb988b9b1b1974e3b7bac03e638
okojomoeko <chikoro.size@gmail.com>
2020-12-19 10:12:45 +0900

    hoge

--- ubuntu2004/shell/setup_anaconda.sh
+++ ubuntu2004/shell/setup_anaconda.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/zsh
 # Install Anaconda for yourself
-wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh -P $1
+if [ ! -e $2/Anaconda3-2020.07-Linux-x86_64.sh ]; then
+  wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh -P $2
 
-expect -c "
+fi
+
+sudo -u $1 expect -c "
 set timeout 360
-spawn sudo /bin/sh $1/Anaconda3-2020.07-Linux-x86_64.sh
+spawn /bin/sh $2/Anaconda3-2020.07-Linux-x86_64.sh
 expect \">>>\"
 send \"\n\"
 expect \":\"
